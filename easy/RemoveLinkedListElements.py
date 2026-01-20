f34db5ad5db7988ab1febf5081c9cdee7e912542
Rahul rc <rc7926422@gmail.com>
2026-01-20 17:05:10 +0530

    Add files via upload

---
+++ easy/RemoveLinkedListElements.py
@@ -0,0 +1,11 @@
+class Soltuion:
+    def removeElement(self, head , val):
+        node = ListNode()
+        node.next = head
+        temp = node
+        while (temp.next!=None):
+            if(temp.next.val==val):
+                temp.next=temp.next.next
+            else:
+                temp=temp.next
+        return node.next
